43346673e1fa399ba317b28736f45932cb01bdd4
kimjong69 <sahilbhosale700@gmail.com>
2024-05-19 22:30:52 +0530

    Commit 1

---
+++ android/app/src/main/kotlin/com/sahilbhosale/clima_weather/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sahilbhosale.clima_weather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test.dart
@@ -0,0 +1,8 @@
+void main() {
+  test();
+}
+
+void test() {
+  String abc = 'hello';
+  print(abc);
+}
